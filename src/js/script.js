ae4cc3a3db2eafa09bd76f682a6b5c5dccc5160e
Sadigzade Hikmet <sadigzade.hi@mail.ru>
2021-04-10 23:47:49 +0300

    Немного продвинулся дальше по верстке данного сайт...

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,25 @@ $(document).ready(function() {
         $('.header-burger, .header-menu').toggleClass('_active');
         $('body').toggleClass('lock');
     });
+    $('.link').click(function(event) {
+        $('.link, .item-spaces__name, .item-spaces__img, .text-block').toggleClass('_active');
+    })
+    $('.introducing-slider').slick({
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 6000,
+    });
+    $('.spaces-slider').slick({
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        // dots: true,
+        // autoplay: true,
+        // autoplaySpeed: 6000,
+    });
 });
